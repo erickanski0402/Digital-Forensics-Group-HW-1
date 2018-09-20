d2d4ba0a37fbbc15bed2107cb1f06df171c1052e
erickanski0402 <37228224+erickanski0402@users.noreply.github.com>
2018-09-20 09:24:37 -0500

    Adding superblock.h, main.c and make
    Adding the rest of the superblock project files

---
+++ main.c
@@ -0,0 +1,25 @@
+#include"superblock.h"
+
+INT1 main(UINT1 argc, CHAR** argv){
+  FILE* pFp;
+
+  if(argc != 2)
+  {
+    printf("Error: No partition specified\n");
+    return -1;
+  }
+
+  //opens user specified partition
+  pFp = fopen(argv[1], "rb");
+
+  if(pFp < 0)
+  {
+    printf("Error: Cannot open specified location\n");
+    return -1;
+  }
+
+  init(pFp);
+  fclose(pFp);
+
+  return 0;
+}
---
+++ superblock.h
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdint.h>
+#include<math.h>
+
+#include"cmntdfs.h"
+
+#define SUPERBLOCK_SIZE 512
+#define SUPERBLOCK_OFFSET 1024
+
+struct superblockValues{
+  UINT4 totalInodes;
+  UINT4 totalBlocks;
+  UINT4 reservedBlocks;
+  UINT4 freeBlocks;
+  UINT4 freeInodes;
+  UINT2 firstUsefulBlock;
+  UINT4 blockSize;
+  UINT4 blocksPerGroup;
+  UINT4 inodesPerGroup;
+  UINT4 magicSignature;
+  UINT4 inodeStructure;
+  UINT4 group;
+  UINT4 preallocate;
+};
+
+struct superblockValues gSuperblock;
+
+INT1 init(FILE*);
+UINT4* superblockGetSuperblockValues(FILE*, UINT4*);
+INT1 superblockSetSuperblockValues(UINT4*);
+UINT4 superblockGetTotalInodes();
+UINT4 superblockGetTotalBlocks();
+UINT4 superblockGetReservedBlocks();
+UINT4 superblockGetFreeBlocks();
+UINT4 superblockGetFreeInodes();
+UINT2 superblockGetFirstUsefulBlock();
+UINT4 superblockGetBlockSize();
+UINT4 superblockGetBlocksPerGroup();
+UINT4 superblockGetInodesPerGroup();
+UINT4 superblockGetMagicSignature();
+UINT4 superblockGetSizeOfInodeStructure();
+UINT4 superblockGetBlockGroup();
+UINT4 superblockGetBlocksToPreallocate();
+INT1 superblockPrintSuperblock();
